bab8eb499afa800d93d00d74175e50992a250d1a
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-24 15:17:22 -0400

    first commit

---
+++ haha/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  haha
+//
+//  Created by Apple on 7/24/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   // func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
+        
+   // }{
+    
+   // }
+
+//}
+
+}
+
+
